549ab699dc5bd9bb91eec2e0e98c00bde64bd402
TheBurntPotato2605 <162242436+TheBurntPotato2605@users.noreply.github.com>
2024-10-09 18:31:08 +0700

    Update LibraryApp.java

--- LibraryApp.java
+++ LibraryApp.java
@@ -17,43 +17,38 @@ public class LibraryApp {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
+    
     Library library = new Library(10);
-    while (true) {
-        System.out.println("\nMenu:");
-        System.out.println("1. Display available books");
-        System.out.println("2. Add a book");
-        System.out.println("3. Borrow a book");
-        System.out.println("4. Return a book");
-        System.out.println("5. Search for a book");
-        System.out.println("6. Display borrowed books");
-        System.out.println("7. Exit");
-        System.out.print("Please enter your choice > "); 
-        int choice = scanner.nextInt();
-        scanner.nextLine(); 
+    while (true) {        
+        int choice =  Validator.inputInt("========MANAGER LIBRARY==========\n"
+                    + "1. Add a book to the library.\n"
+                    + "2. Display all books in the library.\n"
+                    + "3. Borrow a book from the library.\n"
+                    + "4. Display the list of borrowed books\n"
+                    + "5. Return borrowed book to the library\n"
+                    + "6. Search for a book in the library\n"
+                    + "7. Exit\n"
+                    + "Enter your choice : ", "Just be 1->7", "Invalid!", 1, 7);
+        
         switch (choice) {
             case 1:
                 library.displayBooks();
                 break;
             case 2:
-                System.out.print("Enter the title of the book to add: ");
-                String newTitle = scanner.nextLine();
+                String newTitle = Validator.inputString("Enter title: ", "title can not empty!", "^(?!\\s*$).+");
                 library.addBook(newTitle);
                 System.out.println("You added: " + newTitle); // Confirmation message
                 break;
             case 3:
-                System.out.print("Enter the title of the book to borrow: ");
-                String borrowTitle = scanner.nextLine();
+                String borrowTitle = Validator.inputString("Enter title: ", "title can not empty!", "^(?!\\s*$).+");
                 library.borrowBook(borrowTitle);
                 break;
             case 4:
-                System.out.print("Enter the title of the book to return: ");
-                String returnTitle = scanner.nextLine();
+                String borrowTitle = Validator.inputString("Enter title: ", "title can not empty!", "^(?!\\s*$).+");
                 library.returnBook(returnTitle);
                 break;
             case 5:
-                System.out.print("Enter the title of the book to search: ");
-                String searchTitle = scanner.nextLine();
+                String searchTitle = Validator.inputString("Enter title: ", "title can not empty!", "^(?!\\s*$).+");
                 library.searchBook(searchTitle);
                 break;
             case 6:
